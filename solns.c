2e43b56ccd28021ad53e56f7daf47099f7f50c8b
Archi836 <85334460+Archi836@users.noreply.github.com>
2021-06-19 12:04:55 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,21 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int n[], int a)
+
+{
+
+  int max_tn = n[0];
+
+  for (int i = 1; i < a; i++)
+
+  {
+
+    if (max_tn < n[i])
+
+      max_tn = n[i];
+
+  }
+
+  return max_tn;
+
+}
