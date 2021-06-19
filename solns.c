53a07d512cd5335efc537cb62fe4cfd9ffde3661
Archi836 <85334460+Archi836@users.noreply.github.com>
2021-06-19 12:10:45 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -57,3 +57,81 @@ int mode(int a[], int n)
     return maxvalue;
 
 }
+
+int factors(int n, int a[]){
+
+  
+
+  int i, j=0;
+
+  int x=n;
+
+  for(i=2;i<=n;i++)
+
+  {
+
+    
+
+    if(n%i==0)
+
+    {
+
+      
+
+      a[j]=i;
+
+     // printf("%d %d %d\n",j,n,a[j]);
+
+      n=n/i;
+
+      
+
+      i=1;
+
+      j++;
+
+      
+
+    } 
+
+  }
+
+  return j;  
+
+}
+
+int min(int a[], int n){
+
+  int i,x=a[0];
+
+  for(i=0;i<n;i++)
+
+  {
+
+      if(x>a[i])
+
+       x=a[i];
+
+    }
+
+   return x;  
+
+}
+
+float average(int a[], int n){ 
+
+   int i ; float sum=0;
+
+   for(i=0;i<n;i++)
+
+  {
+
+    sum+=a[i]; 
+
+  }
+
+  sum=sum/n;
+
+  return sum;
+
+  }
