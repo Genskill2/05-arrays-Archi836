852f3da4a01c501306033ee486ab0d079136c040
Archi836 <85334460+Archi836@users.noreply.github.com>
2021-06-19 12:07:34 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -19,3 +19,41 @@ int max(int n[], int a)
   return max_tn;
 
 }
+
+int mode(int a[], int n)
+
+{
+
+    int maxcount = 0, maxvalue = 0;
+
+    for (int i = 0; i < n; i++)
+
+    {
+
+        int count = 0;
+
+        for (int j = 0; j < n; j++)
+
+        {
+
+            if (a[i] == a[j])
+
+                count = count + 1;
+
+        }
+
+        if (count > maxcount)
+
+        {
+
+            maxcount = count;
+
+            maxvalue = a[i];
+
+        }
+
+    }
+
+    return maxvalue;
+
+}
